72b391c5fed53ae82e8877b9549d721b8b7eea51
Anna Jamroz <jamroz.anna95@gmail.com>
2023-08-21 20:20:16 +0200

    fixed layout for task, fixed prettier

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,36 +1,30 @@
-import React from 'react';
+import React from "react";
 import "./Task.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faCheck} from '@fortawesome/free-solid-svg-icons';
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Task = (props) => {
+  // destrukturyzacja
+  const { text, date, id, active, finishDate } = props.task;
 
-    // destrukturyzacja
-    const {text, date, id, active, finishDate} = props.task
+  return (
+    <div className="task">
+      <p>
+        {text}
+        {active ? ` - do ${date}` : ` wykonano: ${finishDate}`}
+      </p>
+      <span>
+        {active && (
+          <button onClick={() => props.changeStatus(id)}>
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+        )}
+        <button onClick={() => props.delete(id)}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </span>
+    </div>
+  );
+};
 
-   
-     if (active){
-        return(
-        <div className='task'>
-            <p>{text} - do {date}</p> 
-            <span >
-                <button onClick={() => props.changeStatus(id)}><FontAwesomeIcon icon={faCheck} /></button>
-                <button onClick={() => props.delete(id)}><FontAwesomeIcon icon={faTrash} /></button>
-            </span>
-        </div>
-     )} else {
-        return(
-         <div className='task'>
-            <p>{text} wykonano: {finishDate} </p>  
-            <span>
-                <button onClick={() => props.delete(id)}><FontAwesomeIcon icon={faTrash} /></button> 
-            </span>
-        </div> 
-        )
-     }
-    }
-
- 
 export default Task;
